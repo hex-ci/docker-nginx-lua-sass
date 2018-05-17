331c94976b6f8c9b6c42d5b6bf68c4a50bbf53cc
Hex <hex@codeigniter.org.cn>
2018-05-17 17:09:36 +0800

    Update body.lua

--- lua/body.lua
+++ lua/body.lua
@@ -49,7 +49,7 @@ function compile(str)
   return str
 end
 
-if ngx.re.match(ngx.header.content_type, [[^text/html]]) then
+if ngx.re.find(ngx.header.content_type, [[^text/html]], 'ijo') then
 
   if ngx.arg[1] ~= '' then
     if ngx.ctx.body == nil then
